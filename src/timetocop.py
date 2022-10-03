da3e2976902929a3dd38448a60595cbd79f69759
Annika Rings <annika.rings@cncb.ox.ac.uk>
2022-10-03 18:06:50 +0100

    added some parameters to survival analysis to make it more generally applicable

--- src/timetocop.py
+++ src/timetocop.py
@@ -8,38 +8,51 @@ from scipy import io
 import statsmodels.api as sm
 import statsmodels.formula.api as smf
 import lifelines
-from lifelines import*
+from lifelines import *
 from lifelines.statistics import multivariate_logrank_test
 
 
-def load_copulation(basepath, filename):
+def load_copulation(basepath, filename, in_frames=True, fps=25, xmin=10.0):
     '''loads the specified file and removes NaNs'''
 
     filepath = os.path.join(basepath, filename)
     df = pd.read_csv(filepath, header=None)
     removed_nan = df.dropna()
     observed = [False if data == "None" else True for data in removed_nan[0]]
-    removed_nan[0][removed_nan[0] == 'None'] = 3600 * 25
-    seconds = [int(frames)/25 for frames in removed_nan[0]]
-    in10min = [600.0 if int(
-        frames)/25 > 600.0 else int(frames)/25 for frames in removed_nan[0]]
-    return seconds, in10min, observed
+    removed_nan[0][removed_nan[0] == 'None'] = 3600 * fps
+    xseconds = xmin * 60.0
+    if in_frames:
+        seconds = [int(frames)/fps for frames in removed_nan[0]]
+        inxmin = [xseconds if int(
+            frames)/fps > xseconds else int(frames)/fps for frames in removed_nan[0]]
+    else:
+        seconds = [int(sec) for sec in removed_nan[0]]
+        inxmin = [xseconds if int(frames) > xseconds else int(
+            frames) for frames in removed_nan[0]]
+
+    return seconds, inxmin, observed
 
 
-def kmf_copulation(seconds, in10min, observed, groupname, in_min=True):
+def kmf_copulation(seconds,  inxmin, observed, groupname, in_min=True):
     '''performs kaplan-Meier-fit'''
     kmf = KaplanMeierFitter(label=groupname)
+    kmf_xmin = KaplanMeierFitter(label=groupname)
     minutes = [s/60.0 for s in seconds]
+    minutes_xmin = [s/60.0 for s in inxmin]
     if in_min:
         kmf.fit(minutes, event_observed=observed)
+        kmf_xmin.fit(minutes_xmin, event_observed=observed)
     else:
         kmf.fit(seconds, event_observed=observed)
-    kmf_10min = KaplanMeierFitter(label=groupname)
-    kmf_10min.fit(in10min, event_observed=observed)
-    return kmf, kmf_10min
+        kmf_xmin.fit(inxmin, event_observed=observed)
+
+    return kmf, kmf_xmin
 
 
-def kmf_plot(basepath, filelist, outfilename, groupnames=[], colors=[['#bb44bb', '#d68ed6'], ['#808080', '#b2b2b2'], ['#808080', '#b2b2b2']], hour=True, xlabel='min'):
+def kmf_plot(basepath, filelist, outfilename, groupnames=[],
+             colors=[['#bb44bb', '#d68ed6'], [
+                 '#808080', '#b2b2b2'], ['#808080', '#b2b2b2']],
+             hour=True, xlabel='min', in_frames=True, fps=25, xmin=10.0):
     '''loads the specified files and performs kaplan-Meier-fit and plot'''
 
     if not groupnames:
@@ -54,14 +67,15 @@ def kmf_plot(basepath, filelist, outfilename, groupnames=[], colors=[['#bb44bb',
     ax.set_ylabel('fraction copulated')
 
     for (filename, (groupname, color)) in zip(filelist, zip(groupnames, colors)):
-        seconds, in10min, observed = load_copulation(basepath, filename)
-        kmf, kmf_10min = kmf_copulation(seconds, in10min, observed, groupname)
+        seconds, inxmin, observed = load_copulation(
+            basepath, filename, in_frames=in_frames, fps=fps, xmin=xmin)
+        kmf, kmf_xmin = kmf_copulation(seconds, inxmin, observed, groupname)
         if hour:
             kmf.plot_cumulative_density(color=color)
             timetocop = timetocop + seconds
         else:
-            kmf_10min.plot_cumulative_density(color=color)
-            timetocop = timetocop + in10min
+            kmf_xmin.plot_cumulative_density(color=color)
+            timetocop = timetocop + inxmin
         observed_all = observed_all + observed
         groupvals = [group for data in observed]
         groups = groups + groupvals
